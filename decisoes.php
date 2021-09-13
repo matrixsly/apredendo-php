8f7f5d623524f6294810d6c89054a638884c7340
alex oliveira <alexoliveiraaraujo2000@hotmail.com>
2021-09-13 12:24:44 -0300

    apredendo sobre deciões no php

---
+++ decisoes.php
@@ -0,0 +1,27 @@
+<?php
+ $nome = 'ale';
+
+ $idade = 20;
+
+$pula_linha = PHP_EOL;
+
+$tipo_dados = gettype($idade);
+
+echo 'voce so pode entra ser for maio que 17 anos';
+
+
+
+ if ($idade > 18 or $nome == 'alex') {
+
+     echo $pula_linha;
+     echo 'voce e maior que 17 anos';
+     echo $pula_linha;
+
+ }
+
+ echo "tipo de dados $tipo_dados";
+ 
+ //or avaliar as duas condições ser uma for verdadeira igual true;
+ //and avaliar as duas condições ser uma for falsa igual false;
+
+
