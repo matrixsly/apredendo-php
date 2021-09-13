9508835fdd4ed87c0cf0fe2475015232ea6e8d43
alex oliveira <alexoliveiraaraujo2000@hotmail.com>
2021-09-13 12:26:27 -0300

    apredendo manipula string

---
+++ manipulacao-string.php
@@ -0,0 +1,14 @@
+<?php
+//criando uma variavel 
+//do tipo string que 
+//receber alex
+
+$nome = 'alex';
+
+$idade = 20;
+// criando uma variavel para pula linha
+$pula_linha = PHP_EOL;
+
+echo "meu nome e $nome ". $pula_linha;
+ echo $pula_linha; 
+echo "minha idade e $idade anos";
