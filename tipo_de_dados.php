4e3e559fe994c59429a418f28e4fcc484608c310
alex oliveira <alexoliveiraaraujo2000@hotmail.com>
2021-09-13 12:23:43 -0300

    entendo os tipos de dados no php

--- tipo_de_dados.php
+++ tipo_de_dados.php
@@ -3,4 +3,16 @@
 
 //dados inteiros
 $soma = 2+2;
+// integer
+
+//string
+$nome = 'alex';
+
+//bolean
+ $verdadeiro = true;
+ $falso = false;
+
+ //decimal
+ $divisao = 10 /3 ;
+ 
 echo gettype($soma);
