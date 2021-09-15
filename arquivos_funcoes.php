72aa62f07818672d4b0d7862fe5d945976b6a489
alex oliveira <alexoliveiraaraujo2000@hotmail.com>
2021-09-14 21:31:18 -0300

    apredendo sobre funçoes e como coloca a mesma em um arquivo separado

---
+++ arquivos_funcoes.php
@@ -0,0 +1,40 @@
+<?php
+
+function  exibeMensagem(string $mensagem)
+{
+    echo $mensagem . PHP_EOL;
+}
+
+function depositar(array $conta, float $valorADepositar): array
+{
+    if ($valorADepositar > 0) {
+        $conta['saldo'] += $valorADepositar;
+    } else {
+        exibeMensagem("Depositos precisam ser positivos");
+    }
+    return $conta;
+}
+
+function sacar(array $conta, float $valorASacar): array
+{
+    if ($valorASacar > $conta['saldo']) {
+        exibeMensagem("Você não tem saldo suficiente");
+    } else {
+        $conta['saldo'] -= $valorASacar;
+    }
+
+    return $conta;
+}
+
+
+function titularComletrasMaiusculas(array &$conta)
+{
+     $conta{'titular'} = mb_strtoupper($conta ['titula']);
+};
+
+function exibeConta(array $conta)
+{
+    # code...
+    list('titular' => $titular, 'saldo' => $saldo ) = $conta;
+    echo "<li>titula: $titula. saldo> $saldo</li>";
+}
